dd4b8fb3d11684e66b72d8bdd7b6d5895d0f4e7e
nnsncl <nuniroland@gmail.com>
2023-12-10 21:55:27 +0100

    Reset implementation with yarn workspaces

--- packages/tokens/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './utils/getToken';
-export const designTokens = './json/design_tokens.json';
-export const designTokensTemplate =
-	'./templates/design_tokens_generator_template.json';
