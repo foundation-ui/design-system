763830b1233adf9fa11c1f7316b502fee7e0f95a
nnsncl <nuniroland@gmail.com>
2024-01-10 20:02:22 +0100

    Add Sequence token methods

--- packages/foundations/src/generators/measurements.ts
+++ packages/foundations/src/generators/measurements.ts
@@ -1,4 +1,4 @@
-import { PXToREM, PXToPT } from "../utils";
+import { RGBAToHEX, calculateContrastScore, PXToREM, PXToPT } from "../";
 import {
   TModularScalesOptions,
   TSequencesOptions,
@@ -46,4 +46,84 @@ export const generateSequences = (options: TSequencesOptions): number[] => {
   }
 
   return sequences;
+};
+export const calculateStackOrder = (index: number, sequence: number[]) => {
+  const midSequenceIndex = sequence.length / 2;
+
+  if (index < midSequenceIndex) return { label: "low", score: 1 };
+  if (index > midSequenceIndex) return { label: "high", score: 3 };
+
+  if (
+    index === midSequenceIndex ||
+    index === Math.floor(midSequenceIndex) ||
+    index === Math.ceil(midSequenceIndex)
+  ) {
+    return { label: "median", score: 2 };
+  }
+
+  return;
+};
+export const getUsages = (contrastScore: any | null, stackOrder?: any) => {
+  const contrastScoreRef = JSON.stringify(Object.values(contrastScore || ""));
+  const scores = {
+    depth: {
+      low: stackOrder?.label === "low",
+      median: stackOrder?.label === "median",
+      high: stackOrder?.label === "high",
+    },
+    opacity: {
+      low: contrastScoreRef.includes("F"),
+      high:
+        (contrastScoreRef.includes("AA") || contrastScoreRef.includes("AAA")) &&
+        !contrastScoreRef.includes("F"),
+    },
+  };
+
+  if (stackOrder) {
+    if (scores.depth.low) return ["base", "toolbar", "dropdown"];
+    if (scores.depth.median) return ["fixed", "off-canvas", "overlay"];
+    if (scores.depth.high) return ["notification", "above-content"];
+  } else {
+    if (scores.opacity.low) return ["backdrop", "transparent"];
+    if (scores.opacity.high) return ["icons", "animations", "layout"];
+  }
+
+  return [];
+};
+export const generateSequenceVariation = ({
+  contrast,
+  sequence,
+  index,
+}: any) => {
+  const stackOrder =
+    index && sequence
+      ? calculateStackOrder(index, sequence)
+      : { label: "low", score: 1 }; // Fallback
+  const contrastScore = {
+    light:
+      contrast &&
+      calculateContrastScore(
+        RGBAToHEX(`rgba(0, 0, 0, ${contrast})`, "ffffff"),
+        "ffffff"
+      ),
+    dark:
+      contrast &&
+      calculateContrastScore(
+        RGBAToHEX(`rgba(255, 255, 255, ${contrast})`, "000000"),
+        "000000"
+      ),
+  };
+
+  const variations = {
+    depth: {
+      usage: getUsages(null, stackOrder),
+      stack_order: stackOrder,
+    },
+    opacity: {
+      usage: getUsages(contrastScore),
+      contrast_score: contrastScore,
+    },
+  };
+
+  return !contrast ? variations.depth : variations.opacity;
 };
