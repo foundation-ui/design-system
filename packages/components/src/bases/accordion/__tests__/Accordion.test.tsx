b2fe7d9d0980c9510caf59655eb110b55dc5ae23
nnsncl <nuniroland@gmail.com>
2024-01-31 19:28:26 +0100

    Update accordion tests

---
+++ packages/components/src/bases/accordion/__tests__/Accordion.test.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { screen, render, fireEvent, waitFor } from "@testing-library/react";
+
+import { Accordion } from "../";
+import { SystemThemeProvider } from "@bsw/ds-core";
+
+describe("Accordion", () => {
+  it("Renders without accessibility violation", async () => {
+    const { container } = render(
+      <SystemThemeProvider>
+        <SystemThemeProvider>
+          <Accordion.Root>
+            <Accordion>
+              <Accordion.Trigger value="1">1</Accordion.Trigger>
+              <Accordion.Content value="1">2</Accordion.Content>
+            </Accordion>
+          </Accordion.Root>
+        </SystemThemeProvider>
+      </SystemThemeProvider>
+    );
+
+    // const ComponentContainer = await axe(container);
+    // expect(ComponentContainer).toHaveNoViolations();
+  });
+  it("Renders as Default with accessibility definition", () => {
+    render(
+      <SystemThemeProvider>
+        <Accordion.Root>
+          <Accordion>
+            <Accordion.Trigger value="1">1</Accordion.Trigger>
+            <Accordion.Content value="1">2</Accordion.Content>
+          </Accordion>
+        </Accordion.Root>
+      </SystemThemeProvider>
+    );
+    // expect(AccordionTriggerComponent).toHaveAttribute("value", "1");
+    // expect(AccordionTriggerComponent).toHaveAttribute("aria-expanded", "false");
+  });
+  it("Fires the defined callback when its clicked", async () => {
+    const onClickCallback = jest.fn();
+    render(
+      <SystemThemeProvider>
+        <Accordion.Root>
+          <Accordion>
+            <Accordion.Trigger
+              onClick={onClickCallback}
+              value="1"
+              name="trigger-1"
+            >
+              Trigger 1
+            </Accordion.Trigger>
+            <Accordion.Trigger value="2" name="trigger-2">
+              Trigger 2
+            </Accordion.Trigger>
+            <Accordion.Content value="1" defaultOpen>
+              Content 1
+            </Accordion.Content>
+            <Accordion.Content value="2">Content 2</Accordion.Content>
+          </Accordion>
+        </Accordion.Root>
+      </SystemThemeProvider>
+    );
+
+    expect(screen.getByText("Content 1")).toBeDefined();
+
+    fireEvent.click(screen.getByLabelText("trigger-2-action"));
+    await waitFor(() => {
+      expect(() => screen.getByText("Content 1")).toThrow();
+      expect(screen.getByText("Content 2")).toBeDefined();
+    });
+
+    fireEvent.click(screen.getByLabelText("trigger-1-action"));
+    await waitFor(() => {
+      expect(() => screen.getByText("Content 2")).toThrow();
+      expect(screen.getByText("Content 1")).toBeDefined();
+      expect(onClickCallback).toHaveBeenCalledTimes(1);
+    });
+
+    fireEvent.click(screen.getByLabelText("trigger-1-action"));
+    await waitFor(() => {
+      expect(() => screen.getByText("Content 1")).toThrow();
+      expect(() => screen.getByText("Content 2")).toThrow();
+    });
+  });
+});
