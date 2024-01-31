af79f110fa49cbfc99eb4963f385d743e472eef0
nnsncl <nuniroland@gmail.com>
2024-01-31 19:58:40 +0100

    Add axe tests to accordion component

--- packages/components/src/bases/accordion/__tests__/Accordion.test.tsx
+++ packages/components/src/bases/accordion/__tests__/Accordion.test.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { screen, render, fireEvent, waitFor } from "@testing-library/react";
+import { axe, toHaveNoViolations } from "jest-axe";
 
 import { Accordion } from "../";
 import { SystemThemeProvider } from "@bsw/ds-core";
 
+expect.extend(toHaveNoViolations);
 describe("Accordion", () => {
   it("Renders without accessibility violation", async () => {
     const { container } = render(
@@ -12,31 +14,45 @@ describe("Accordion", () => {
           <Accordion.Root>
             <Accordion>
               <Accordion.Trigger value="1">1</Accordion.Trigger>
-              <Accordion.Content value="1">2</Accordion.Content>
+              <Accordion.Content value="1" defaultOpen>
+                2
+              </Accordion.Content>
             </Accordion>
           </Accordion.Root>
         </SystemThemeProvider>
       </SystemThemeProvider>
     );
 
-    // const ComponentContainer = await axe(container);
-    // expect(ComponentContainer).toHaveNoViolations();
+    const ComponentContainer = await axe(container);
+    expect(ComponentContainer).toHaveNoViolations();
   });
-  it("Renders as Default with accessibility definition", () => {
+  it("Renders as Default with accessibility definition", async () => {
     render(
       <SystemThemeProvider>
         <Accordion.Root>
           <Accordion>
             <Accordion.Trigger value="1">1</Accordion.Trigger>
-            <Accordion.Content value="1">2</Accordion.Content>
+            <Accordion.Content value="1">1</Accordion.Content>
           </Accordion>
         </Accordion.Root>
       </SystemThemeProvider>
     );
-    // expect(AccordionTriggerComponent).toHaveAttribute("value", "1");
-    // expect(AccordionTriggerComponent).toHaveAttribute("aria-expanded", "false");
+    const Trigger = screen.getByLabelText("button-action");
+
+    expect(Trigger).toBeDefined();
+    expect(() => screen.getByRole("region")).toThrow();
+
+    fireEvent.click(Trigger);
+    await waitFor(() => {
+      const Content = screen.getByRole("region");
+      expect(Content).toBeDefined();
+      expect(Content.getAttribute("aria-expanded")).toBe("true");
+      expect(Content.getAttribute("data-value")).toBe("1");
+      expect(Content.getAttribute("aria-labelledby")).toBeDefined();
+      expect(Content.getAttribute("id")).toBeDefined();
+    });
   });
-  it("Fires the defined callback when its clicked", async () => {
+  it("Fires the defined callback function and toggle the content when the trigger is clicked", async () => {
     const onClickCallback = jest.fn();
     render(
       <SystemThemeProvider>
