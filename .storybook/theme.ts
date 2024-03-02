5afb7ccb92e16883d57232a367f687e79863dece
nnsncl <nuniroland@gmail.com>
2024-03-02 20:07:17 +0100

    Setup sb theming

---
+++ .storybook/theme.ts
@@ -0,0 +1,26 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  brandTitle: "Foundation",
+  brandUrl: "https://github.com/foundation-ui",
+  brandTarget: "_blank",
+
+  // UI
+  appBg: "#212121",
+  appContentBg: "#212121",
+  appPreviewBg: "#212121",
+  appBorderColor: "#111111",
+  appBorderRadius: 4,
+
+  // Text colors
+  textColor: "#FAFAFA",
+
+  // Toolbar default and active colors
+  barSelectedColor: "#111111",
+  barBg: "#212121",
+
+  // Form colors
+  inputBg: "#212121",
+  inputBorder: "#FFFFFF10",
+});
