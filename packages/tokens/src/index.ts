cec888b6b5702b7ae974c2d6f891f523e4a8dbad
nnsncl <nuniroland@gmail.com>
2024-03-27 18:06:31 +0100

    Divide design tokens assets

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,8 +1,24 @@
+/** Contexts */
+export * from "./contexts/DesignTokensProvider";
+export * from "./services/get-token";
+
+/** Global */
 export { default as js_design_tokens } from "./assets/js/ds-tokens";
 export { default as json_design_tokens } from "./assets/json/ds-tokens.json";
 export { default as json_design_tokens_template } from "./assets/templates/dt_template.json";
 
-export { design_system_themes } from "./themes";
+/** Assets */
+export { default as colors_dark_design_tokens } from "./assets/json/color/dark.json";
+export { default as colors_light_design_tokens } from "./assets/json/color/light.json";
+export { default as colors_system_design_tokens } from "./assets/json/color/system.json";
 
-export * from "./contexts/DesignTokensProvider";
-export * from "./services/get-token";
+export { default as measurement_standard_design_tokens } from "./assets/json/measurement/standard.json";
+export { default as fontsize_standard_design_tokens } from "./assets/json/fontsize/standard.json";
+
+export { default as depth_standard_design_tokens } from "./assets/json/depth/standard.json";
+export { default as depth_extended_design_tokens } from "./assets/json/depth/extended.json";
+export { default as opacity_standard_design_tokens } from "./assets/json/opacity/standard.json";
+export { default as opacity_extended_design_tokens } from "./assets/json/opacity/extended.json";
+
+/** Themes */
+export { design_system_themes } from "./themes";
