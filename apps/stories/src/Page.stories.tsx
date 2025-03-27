d0974411b33535a963ed7d1546244d223d4ded16
nnsncl <nuniroland@gmail.com>
2025-03-27 21:23:05 +0100

    Remove dev components

--- apps/stories/src/Page.stories.tsx
+++ apps/stories/src/Page.stories.tsx
@@ -1,29 +1,19 @@
 import React from "react";
-import type { Meta } from "@storybook/react";
-import { ColorModeContext } from "@foundation-ui/tokens";
+
+import { useColorMode } from "@foundation-ui/tokens";
 import {
   Page,
   Button,
   Avatar,
-  Sheet,
   Dialog,
   Badge,
-  Toolbar,
-  Portal,
+  Tooltip,
 } from "@foundation-ui/components";
 
-import { AppProvider } from "./contexts/AppProvider";
-import { AppSettings } from "./components/settings";
-import { QuickActionsMenu } from "./components/layouts/Menus";
-import {
-  ExternalLinksNavigation,
-  InternalActionsNavigation,
-} from "./components/layouts/Navigations";
-
-import settings from "./mocks/settings.json";
+import type { Meta } from "@storybook/react";
 
 const meta = {
-  title: "Modules/Landing",
+  title: "Misc/PlaygroundPage",
   component: Page,
 } satisfies Meta<typeof Page>;
 
@@ -35,14 +25,9 @@ const DisplayXl = styled.h1`
   line-height: 1;
   font-weight: 600;
 `;
-const DemoContainer = styled.div`
-  padding: 0 var(--measurement-medium-30);
-  border-radius: var(--measurement-medium-30);
-  border: var(--measurement-small-30) solid var(--font-color-alpha-10);
-  background: var(--body-color);
-`;
+
 const Navbar = () => {
-  const { colorMode, setColorMode } = React.useContext(ColorModeContext);
+  const { colorMode, setColorMode } = useColorMode();
 
   return (
     <nav
@@ -86,31 +71,34 @@ const Navbar = () => {
             <path d="M409.132 114.573c-19.608-33.596-46.205-60.194-79.798-79.8-33.598-19.607-70.277-29.408-110.063-29.408-39.781 0-76.472 9.804-110.063 29.408-33.596 19.605-60.192 46.204-79.8 79.8C9.803 148.168 0 184.854 0 224.63c0 47.78 13.94 90.745 41.827 128.906 27.884 38.164 63.906 64.572 108.063 79.227 5.14.954 8.945.283 11.419-1.996 2.475-2.282 3.711-5.14 3.711-8.562 0-.571-.049-5.708-.144-15.417a2549.81 2549.81 0 01-.144-25.406l-6.567 1.136c-4.187.767-9.469 1.092-15.846 1-6.374-.089-12.991-.757-19.842-1.999-6.854-1.231-13.229-4.086-19.13-8.559-5.898-4.473-10.085-10.328-12.56-17.556l-2.855-6.57c-1.903-4.374-4.899-9.233-8.992-14.559-4.093-5.331-8.232-8.945-12.419-10.848l-1.999-1.431c-1.332-.951-2.568-2.098-3.711-3.429-1.142-1.331-1.997-2.663-2.568-3.997-.572-1.335-.098-2.43 1.427-3.289 1.525-.859 4.281-1.276 8.28-1.276l5.708.853c3.807.763 8.516 3.042 14.133 6.851 5.614 3.806 10.229 8.754 13.846 14.842 4.38 7.806 9.657 13.754 15.846 17.847 6.184 4.093 12.419 6.136 18.699 6.136 6.28 0 11.704-.476 16.274-1.423 4.565-.952 8.848-2.383 12.847-4.285 1.713-12.758 6.377-22.559 13.988-29.41-10.848-1.14-20.601-2.857-29.264-5.14-8.658-2.286-17.605-5.996-26.835-11.14-9.235-5.137-16.896-11.516-22.985-19.126-6.09-7.614-11.088-17.61-14.987-29.979-3.901-12.374-5.852-26.648-5.852-42.826 0-23.035 7.52-42.637 22.557-58.817-7.044-17.318-6.379-36.732 1.997-58.24 5.52-1.715 13.706-.428 24.554 3.853 10.85 4.283 18.794 7.952 23.84 10.994 5.046 3.041 9.089 5.618 12.135 7.708 17.705-4.947 35.976-7.421 54.818-7.421s37.117 2.474 54.823 7.421l10.849-6.849c7.419-4.57 16.18-8.758 26.262-12.565 10.088-3.805 17.802-4.853 23.134-3.138 8.562 21.509 9.325 40.922 2.279 58.24 15.036 16.18 22.559 35.787 22.559 58.817 0 16.178-1.958 30.497-5.853 42.966-3.9 12.471-8.941 22.457-15.125 29.979-6.191 7.521-13.901 13.85-23.131 18.986-9.232 5.14-18.182 8.85-26.84 11.136-8.662 2.286-18.415 4.004-29.263 5.146 9.894 8.562 14.842 22.077 14.842 40.539v60.237c0 3.422 1.19 6.279 3.572 8.562 2.379 2.279 6.136 2.95 11.276 1.995 44.163-14.653 80.185-41.062 108.068-79.226 27.88-38.161 41.825-81.126 41.825-128.906-.01-39.771-9.818-76.454-29.414-110.049z"></path>
           </svg>
         </Button>
-        <Button
-          className="flex align-center justify-between"
-          variant="ghost"
-          onClick={() => setColorMode(colorMode === "dark" ? "light" : "dark")}
-        >
-          <svg
-            viewBox="0 0 24 24"
-            focusable="false"
-            aria-hidden="true"
-            width={24}
-            height={24}
-            fill="currentColor"
+        <Tooltip content={`Mode: ${colorMode === "dark" ? "Dark" : "Light"}`}>
+          <Button
+            className="flex align-center justify-between"
+            variant="ghost"
+            onClick={() =>
+              setColorMode(colorMode === "dark" ? "light" : "dark")
+            }
           >
-            {colorMode === "dark" ? (
-              <path d="m6.76 4.84-1.8-1.79-1.41 1.41 1.79 1.79zM4 10.5H1v2h3zm9-9.95h-2V3.5h2zm7.45 3.91-1.41-1.41-1.79 1.79 1.41 1.41zm-3.21 13.7 1.79 1.8 1.41-1.41-1.8-1.79zM20 10.5v2h3v-2zm-8-5c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6m-1 16.95h2V19.5h-2zm-7.45-3.91 1.41 1.41 1.79-1.8-1.41-1.41z" />
-            ) : (
-              <path d="M12 3c-4.97 0-9 4.03-9 9s4.03 9 9 9 9-4.03 9-9c0-.46-.04-.92-.1-1.36-.98 1.37-2.58 2.26-4.4 2.26-2.98 0-5.4-2.42-5.4-5.4 0-1.81.89-3.42 2.26-4.4-.44-.06-.9-.1-1.36-.1" />
-            )}
-          </svg>
-        </Button>
+            <svg
+              viewBox="0 0 24 24"
+              focusable="false"
+              aria-hidden="true"
+              width={18}
+              height={18}
+              fill="currentColor"
+            >
+              {colorMode === "dark" ? (
+                <path d="m6.76 4.84-1.8-1.79-1.41 1.41 1.79 1.79zM4 10.5H1v2h3zm9-9.95h-2V3.5h2zm7.45 3.91-1.41-1.41-1.79 1.79 1.41 1.41zm-3.21 13.7 1.79 1.8 1.41-1.41-1.8-1.79zM20 10.5v2h3v-2zm-8-5c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6m-1 16.95h2V19.5h-2zm-7.45-3.91 1.41 1.41 1.79-1.8-1.41-1.41z" />
+              ) : (
+                <path d="M12 3c-4.97 0-9 4.03-9 9s4.03 9 9 9 9-4.03 9-9c0-.46-.04-.92-.1-1.36-.98 1.37-2.58 2.26-4.4 2.26-2.98 0-5.4-2.42-5.4-5.4 0-1.81.89-3.42 2.26-4.4-.44-.06-.9-.1-1.36-.1" />
+              )}
+            </svg>
+          </Button>
+        </Tooltip>
       </div>
     </nav>
   );
 };
-
 const Heading = () => {
   return (
     <hgroup className="grid g-medium-60" style={{ maxWidth: 1024 }}>
@@ -118,12 +106,20 @@ const Heading = () => {
         Build products and digital experiences with Data-Driven insights and
         components optimized for fast development.
       </DisplayXl>
-      <p className="fs-medium-40 opacity-default-60" style={{ maxWidth: 768 }}>
-        Foundation offers a suite of powerful Engines and Libraries, optimized
-        to create cohesive UIs with agnostic Design Tokens and Themes. It
-        provides accessible, robust, and intuitive components alongside User
-        Behavior Analytics that can be programmatically integrated, ensuring
-        seamless, data-driven design and development.
+      <p className="fs-medium-40" style={{ maxWidth: 768 }}>
+        Foundation offers a suite of powerful&nbsp;
+        <Tooltip content="@fui/core">
+          <b>Engines</b>
+        </Tooltip>
+        &nbsp;and Libraries, optimized to create cohesive UIs with
+        agnostic&nbsp;
+        <Tooltip content="@fui/tokens">
+          <b>Design Tokens</b>
+        </Tooltip>
+        &nbsp;and Themes. It provides accessible, robust, and intuitive
+        components alongside User Behavior Analytics that can be
+        programmatically integrated, ensuring seamless, data-driven design and
+        development.
       </p>
       <div className="flex g-medium-60 align-center">
         <Button variant="primary">Get started</Button>
@@ -138,59 +134,6 @@ const Heading = () => {
         </Button>
       </div>
     </hgroup>
-  );
-};
-const Demo = () => {
-  const { idb, uba, ab, app } = settings;
-
-  return (
-    <AppProvider idb={idb} uba={uba} ab={ab} app={app}>
-      <Sheet.Root>
-        <Portal container="dialog-portal">
-          <Sheet side="right" sizing="large">
-            <hgroup className="grid g-medium-30">
-              <h3>AB</h3>
-            </hgroup>
-          </Sheet>
-        </Portal>
-
-        <DemoContainer>
-          <AppSettings />
-          <Page.Menu className="flex justify-between align-center">
-            <Avatar
-              alt="gh-logo"
-              src="https://avatars.githubusercontent.com/u/59123840?v=4"
-              sizing="small"
-            />
-
-            <div className="flex g-medium-30 align-center">
-              <Button variant="mono">Save</Button>
-              <Button variant="mono">Code</Button>
-              <Button variant="mono">Share</Button>
-              <Sheet.Trigger sizing="medium" variant="mono">
-                <svg viewBox="0 0 15 15" fill="currentColor">
-                  <path
-                    d="M3.625 7.5C3.625 8.12132 3.12132 8.625 2.5 8.625C1.87868 8.625 1.375 8.12132 1.375 7.5C1.375 6.87868 1.87868 6.375 2.5 6.375C3.12132 6.375 3.625 6.87868 3.625 7.5ZM8.625 7.5C8.625 8.12132 8.12132 8.625 7.5 8.625C6.87868 8.625 6.375 8.12132 6.375 7.5C6.375 6.87868 6.87868 6.375 7.5 6.375C8.12132 6.375 8.625 6.87868 8.625 7.5ZM12.5 8.625C13.1213 8.625 13.625 8.12132 13.625 7.5C13.625 6.87868 13.1213 6.375 12.5 6.375C11.8787 6.375 11.375 6.87868 11.375 7.5C11.375 8.12132 11.8787 8.625 12.5 8.625Z"
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </Sheet.Trigger>
-            </div>
-          </Page.Menu>
-
-          <section
-            className="grid g-medium-60 p-x-medium-30 p-y-medium-80"
-            style={{ aspectRatio: 2 }}
-          >
-            <div className="flex justify-between g-medium-30">
-              <h2>Analytics</h2>
-              <Button variant="primary">Download</Button>
-            </div>
-          </section>
-        </DemoContainer>
-      </Sheet.Root>
-    </AppProvider>
   );
 };
 const Footer = () => {
@@ -285,7 +228,7 @@ const Footer = () => {
   );
 };
 
-export const Landing = {
+export const PlaygroundPage = {
   render: () => {
     return (
       <Dialog.Root>
@@ -293,11 +236,10 @@ export const Landing = {
           <Page.Content>
             <Navbar />
             <section
-              className="p-x-medium-60 p-b-medium-60 p-t-large-90 grid g-medium-90 align-center"
+              className="p-x-medium-60  grid g-medium-90 align-center h-95"
               style={{ maxWidth: 1440, margin: "0 auto" }}
             >
               <Heading />
-              <Demo />
             </section>
             <Footer />
           </Page.Content>
